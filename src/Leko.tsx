d6f12b69a1137460e873949e303f794c34efef4c
formicant <formicant@gmail.com>
2021-09-15 00:16:16 +0700

    lukin pi leko pi kulupu ante.

--- src/Leko.tsx
+++ src/Leko.tsx
@@ -1,10 +1,13 @@
 import './Leko.css';
+import classNames from 'classnames';
 import { Component } from 'react';
 import { Lon } from './Lon';
 import { Kule } from './Kule';
-import { Nimi } from './NimiAli'
+import { Nimi, KlupuNimi, kulupuPiNimiAli } from './NimiAli'
 import { lukinPiNnimiAli } from './LukinNimi'
 import { kipisiENimi } from './KipisiENimi';
+
+type kulupuLeko = KlupuNimi | 'sitelen';
 
 export interface JoLeko extends Lon
 {
@@ -18,14 +21,29 @@ export abstract class Leko extends Component<JoLeko, any>
     super(props);
   }
   
-  render = () =>
-    <div className='Leko' style={{
-      color: this.kule(),
+  render()
+  {
+    const kulupu = this.kulupu();
+    const kule = this.kule();
+    
+    const kulupuLukin = classNames('Leko', this.kulupu());
+    const lukinKule = kulupu === 'ijo' || kulupu === 'toki'
+      ? { backgroundColor: kule }
+      : { color: kule };
+    const lukin = {
       left: `${this.props.x}em`,
       top: `${this.props.y}em`,
-    }}>
-      {this.insa()}
-    </div>;
+      ...lukinKule
+    };
+    
+    return (
+      <div className={kulupuLukin} style={lukin}>
+        {this.insa()}
+      </div>
+    );
+  };
+  
+  protected abstract kulupu(): kulupuLeko;
   
   protected abstract insa(): JSX.Element | JSX.Element[];
   
@@ -38,23 +56,27 @@ export abstract class Leko extends Component<JoLeko, any>
 
 export class LekoSitelen extends Leko
 {
+  protected kulupu = () => 'sitelen' as const;
+  
   protected insa = () =>
-    <span className='sitelen'>
+    <span>
       {this.props.nimi}
     </span>;
 }
 
 export class LekoNimi extends Leko
 {
+  protected kulupu =
+    () => kulupuPiNimiAli[this.props.nimi];
+  
   protected insa()
   {
     const linja = kipisiENimi(this.props.nimi);
     const nanpaLinja = linja.length === 1 ? 'Wan' : 'Tu';
+    const kulupuLukin = classNames('nimi', `linja${nanpaLinja}`);
     
     return linja.map(({ nimiLili, suliPoka }, nanpa) =>
-      <span
-        key={nanpa}
-        className={`nimi linja${nanpaLinja}`}
+      <span key={nanpa} className={kulupuLukin}
         style={{ fontVariationSettings: `'wdth' ${suliPoka}, 'opsz' 12` }}
       >
         {nimiLili}
