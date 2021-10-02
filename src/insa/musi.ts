9294f305d122ea87e7d314fcc905a5d7a5d51efa
formicant <formicant@gmail.com>
2021-10-02 22:08:00 +0700

    pona e Musi.

--- src/insa/musi.ts
+++ src/insa/musi.ts
@@ -7,110 +7,101 @@ import { NimiIjo, panaEKulupuNimi } from "./nimiAli";
 import { panaENasinMusiAli } from "./pilinToki";
 import { Tawa, tawaOpen, panaEKulupuTawa } from "./tawa";
 
-export interface Musi
+interface IMusi
 {
   readonly nimiMa: string;
   readonly suliMa: Lon;
   readonly tenpo: Im.List<Tawa>;
   readonly tenpoNi: number;
 }
+const musiAla: IMusi = { nimiMa: '', suliMa: new Lon(NaN, NaN), tenpo: Im.List(), tenpoNi: NaN };
 
-export function openEMusi(nimiMa: string, suliMa: Lon, ijoAli: readonly Ijo[]): Musi
+export class Musi extends Im.Record<IMusi>(musiAla) implements IMusi
 {
-  return {
-    nimiMa: nimiMa,
-    suliMa: suliMa,
-    tenpo: Im.List.of(tawaOpen(suliMa, ijoAli)),
-    tenpoNi: 0,
-  };
-}
-
-export function tenpoMonsi(musi: Musi, ali: boolean = false): Musi
-{
-  if(musi.tenpoNi > 0)
-    return { ...musi, tenpoNi: ali ? 0 : musi.tenpoNi - 1 };
-  else
-    return musi;
-}
-
-export function tenpoSinpin(musi: Musi, ali: boolean = false): Musi
-{
-  if(musi.tenpoNi < musi.tenpo.size - 1)
-    return { ...musi, tenpoNi: ali ? musi.tenpo.size - 1 : musi.tenpoNi + 1 };
-  else
-    return musi;
-}
-
-export function tawa(musi: Musi, nasin: NasinTawa): Musi
-{
-  const lipuIjo = tawaNi(musi).lipuIjo;
-  const lonIjo = paliELonIjo(lipuIjo);
-  const nasinMusi = panaENasinMusiAli(musi.suliMa, lonIjo);
-  const lonPali = paliELonPali(lonIjo, nasinMusi);
-  
-  const kulupuTawa = panaEKulupuTawa(musi.suliMa, lonPali, nasin)
-    .toKeyedSeq()
-    .mapEntries(([_, nanpa]) => [nanpa, lipuIjo.get(nanpa)!]);
-  
-  const kulupuTawaSin = kulupuTawa.map(ijo => ijo.tawa(nasin));
-  const lipuIjoSin = lipuIjo.merge(kulupuTawaSin);
-  
-  const lonIjoSin = paliELonIjo(lipuIjoSin);
-  const nasinMusiSin = panaENasinMusiAli(musi.suliMa, lonIjoSin);
-  const lonPaliSin = paliELonPali(lonIjoSin, nasinMusiSin);
-  const ijoAnte = panaEIjoAnte(lonPaliSin, lipuIjoSin);
-  
-  const lipuIjoAnte = lipuIjoSin.merge(ijoAnte);
-  
-  const lonIjoAnte = paliELonIjo(lipuIjoAnte);
-  const nasinMusiAnte = panaENasinMusiAli(musi.suliMa, lonIjoAnte);
-  const lonPaliAnte = paliELonPali(lonIjoAnte, nasinMusiAnte);
-  
-  // O PALI: ken ante e ijo wan tawa ijo mute!
-  const lukinWawa = Im.Seq(nasinMusiAnte).flatMap(nasin => nasin.nanpaIjo)
-    .concat(lonPaliAnte.valueSeq().flatMap(mute => mute.filter((pali, nanpa) => lipuIjoAnte.get(nanpa)!.liSitelen() && !pali.isEmpty()).keySeq()))
-    .toSet();
-  
-  if(lipuIjoAnte.equals(lipuIjo)) // li pali ala. ijo li sama ala.
-    return musi
-  else
+  constructor(nimiMa: string, suliMa: Lon, ijoAli: readonly Ijo[])
   {
-    const tawaSin: Tawa = new Tawa({ nasin: nasin, lipuIjo: lipuIjoAnte, lukinWawa: lukinWawa });
-    const tenpoSin = musi.tenpo.toSeq().take(musi.tenpoNi + 1).concat([tawaSin]).toList();
+    super({
+      nimiMa: nimiMa,
+      suliMa: suliMa,
+      tenpo: Im.List.of(tawaOpen(suliMa, ijoAli)),
+      tenpoNi: 0,
+    });
+  }
+
+  get tawaNi()
+  {
+    const ni = this.tenpo.get(this.tenpoNi);
+    if(ni) return ni;
+    else throw Error('tenpo ni li ike!');
+  }
+  
+  tenpoMonsi(ali: boolean = false): Musi
+  {
+    if(this.tenpoNi > 0)
+      return this.merge({ tenpoNi: ali ? 0 : this.tenpoNi - 1 });
+    else
+      return this;
+  }
+
+  tenpoSinpin(ali: boolean = false): Musi
+  {
+    if(this.tenpoNi < this.tenpo.size - 1)
+      return this.merge({ tenpoNi: ali ? this.tenpo.size - 1 : this.tenpoNi + 1 });
+    else
+      return this;
+  }
+
+  tawa(nasin: NasinTawa): Musi
+  {
+    const lipuIjo = this.tawaNi.lipuIjo;
+    const lonIjo = paliELonIjo(lipuIjo);
+    const nasinMusi = panaENasinMusiAli(this.suliMa, lonIjo);
+    const lonPali = paliELonPali(lonIjo, nasinMusi);
     
-    return { ...musi, tenpo: tenpoSin, tenpoNi: musi.tenpoNi + 1 };
+    const kulupuTawa = panaEKulupuTawa(this.suliMa, lonPali, nasin)
+      .toKeyedSeq()
+      .mapEntries(([_, nanpa]) => [nanpa, lipuIjo.get(nanpa)!]);
+    
+    const kulupuTawaSin = kulupuTawa.map(ijo => ijo.tawa(nasin));
+    const lipuIjoSin = lipuIjo.merge(kulupuTawaSin);
+    
+    const lonIjoSin = paliELonIjo(lipuIjoSin);
+    const nasinMusiSin = panaENasinMusiAli(this.suliMa, lonIjoSin);
+    const lonPaliSin = paliELonPali(lonIjoSin, nasinMusiSin);
+    const ijoAnte = this.panaEIjoAnte(lonPaliSin, lipuIjoSin);
+    
+    const lipuIjoAnte = lipuIjoSin.merge(ijoAnte);
+    
+    const lonIjoAnte = paliELonIjo(lipuIjoAnte);
+    const nasinMusiAnte = panaENasinMusiAli(this.suliMa, lonIjoAnte);
+    const lonPaliAnte = paliELonPali(lonIjoAnte, nasinMusiAnte);
+    
+    // O PALI: ken ante e ijo wan tawa ijo mute!
+    const lukinWawa = Im.Seq(nasinMusiAnte).flatMap(nasin => nasin.nanpaIjo)
+      .concat(lonPaliAnte.valueSeq().flatMap(mute => mute.filter((pali, nanpa) => lipuIjoAnte.get(nanpa)!.liSitelen() && !pali.isEmpty()).keySeq()))
+      .toSet();
+    
+    if(lipuIjoAnte.equals(lipuIjo))
+      return this
+    else
+    {
+      const tawaSin: Tawa = new Tawa({ nasin: nasin, lipuIjo: lipuIjoAnte, lukinWawa: lukinWawa });
+      const tenpoSin = this.tenpo.take(this.tenpoNi + 1).push(tawaSin);
+      
+      return this.merge({ tenpo: tenpoSin, tenpoNi: this.tenpoNi + 1 });
+    }
+  }
+
+  private panaEIjoAnte(lonPali: LonPali, lipuIjo: LipuIjo)
+  {
+    return Im.Seq.Keyed(
+      lonPali.entrySeq().flatMap(([_, mute]) =>
+        mute
+          .map(paliMute => paliMute.filter(pali => panaEKulupuNimi(pali) === 'ijo') as Im.Set<NimiIjo>)
+          .filterNot(paliMute => paliMute.isEmpty())
+          .toKeyedSeq()
+      )
+    ).map((anteMute, nanpa) => lipuIjo.get(nanpa)!.merge({ kulupu: 'sitelen', nimi: anteMute.first()! }));
   }
 }
 
-function panaEIjoAnte(lonPali: LonPali, lipuIjo: LipuIjo)
-{
-  return Im.Seq.Keyed(
-    lonPali.entrySeq().flatMap(([_, mute]) =>
-      mute
-        .map(paliMute => paliMute.filter(pali => panaEKulupuNimi(pali) === 'ijo') as Im.Set<NimiIjo>)
-        .filterNot(paliMute => paliMute.isEmpty())
-        .toKeyedSeq()
-    )
-  ).map((anteMute, nanpa) => lipuIjo.get(nanpa)!.merge({ kulupu: 'sitelen', nimi: anteMute.first()! }));
-}
-
-export function tawaNi(musi: Musi)
-{
-  const tawaNi = musi.tenpo.get(musi.tenpoNi);
-  if(tawaNi)
-    return tawaNi;
-  else
-    throw Error('tenpo ni li ike!');
-}
-
-// /**
-//  * pana e linja tenpo pi ijo wan tan tenpo ni tawa open musi.
-//  * ni li pona taso tawa jasima e leko.
-//  * @param nanpaIjo nanpa taso tawa pana e ijo tan LipuIjo.
-//  */
-// export function tenpoPiniIjo(musi: Musi, nanpaIjo: number)
-// {
-//   return Im.Range(musi.tenpoNi, -1, -1)
-//     .map(t => musi.tenpo.get(t)!.lipuIjo.get(nanpaIjo))
-//     .takeWhile(t => t !== undefined) as Im.Seq.Indexed<Ijo>;
-// }
--- src/lukinMusi.tsx
+++ src/lukinMusi.tsx
@@ -1,33 +1,34 @@
 import './lukinMusi.css';
 import lipuMaAli from './lipu/lipuMa.json'
 import { Component } from 'react';
-import { Musi, tawaNi, openEMusi, tenpoMonsi, tenpoSinpin, tawa } from './insa/musi';
+import { Musi } from './insa/musi';
 import { IloPalisa, Palisa } from './iloPalisa';
 import { Ma } from './ma';
 import { pilinELipuMa } from './insa/lipuMa';
 
-export class LukinMusi extends Component<{ }, Musi>
+export class LukinMusi extends Component<{ }, { musi: Musi }>
 {
   constructor(jo: { })
   {
     super(jo);
     const lipuMa = pilinELipuMa(lipuMaAli[1]);
-    this.state = openEMusi(lipuMa.nimiMa, lipuMa.suliMa, lipuMa.ijoAli);
+    this.state = { musi: new Musi(lipuMa.nimiMa, lipuMa.suliMa, lipuMa.ijoAli) };
   }
   
   render()
   {
-    const ni = tawaNi(this.state);
+    const musi = this.state.musi;
+    const tawaNi = musi.tawaNi;
     
     return (
       <main>
         <div className='nimiMa'>
-          {this.state.nimiMa}
+          {musi.nimiMa}
         </div>
         <Ma
-          suli={this.state.suliMa}
-          lipuIjo={ni.lipuIjo}
-          lukinWawa={ni.lukinWawa}
+          suli={musi.suliMa}
+          lipuIjo={tawaNi.lipuIjo}
+          lukinWawa={tawaNi.lukinWawa}
         />
         <IloPalisa palisaLa={this.lukaPalisa} />
       </main>
@@ -37,18 +38,18 @@ export class LukinMusi extends Component<{ }, Musi>
   private lukaPalisa = (palisa: Palisa) =>
   {
     const pali = this.paliPalisa[palisa];
-    this.setState(pali(this.state));
+    this.setState({ musi: pali(this.state.musi) });
   };
   
   private paliPalisa: { [palisa in Palisa]: (musi: Musi) => Musi } =
   {
-    'sewi':   musi => tawa(musi, 'sewi'),
-    'anpa':   musi => tawa(musi, 'anpa'),
-    'soto':   musi => tawa(musi, 'soto'),
-    'teje':   musi => tawa(musi, 'teje'),
-    'monsi':  musi => tenpoMonsi(musi),
-    'sinpin': musi => tenpoSinpin(musi),
-    'open':   musi => tenpoMonsi(musi, true),
-    'pini':   musi => tenpoSinpin(musi, true),
+    'sewi':   musi => musi.tawa('sewi'),
+    'anpa':   musi => musi.tawa('anpa'),
+    'soto':   musi => musi.tawa('soto'),
+    'teje':   musi => musi.tawa('teje'),
+    'monsi':  musi => musi.tenpoMonsi(),
+    'sinpin': musi => musi.tenpoSinpin(),
+    'open':   musi => musi.tenpoMonsi(true),
+    'pini':   musi => musi.tenpoSinpin(true),
   } as const;
 }
